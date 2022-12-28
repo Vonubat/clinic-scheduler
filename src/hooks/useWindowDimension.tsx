c12e12fc9a6871a652cc7c5d46fdea598663e1c1
Tabunov Egor <Vonubat@gmail.com>
2022-12-28 18:14:15 +0300

    refactor: delete lodash && implement own debounce fn

--- src/hooks/useWindowDimension.tsx
+++ src/hooks/useWindowDimension.tsx
@@ -1,6 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React, { useEffect, useState } from 'react';
-import { debounce } from 'lodash';
 
 export const useWindowDimension = (): number[] => {
   const [dimension, setDimension] = useState([window.innerWidth, window.innerHeight]);
@@ -8,9 +7,17 @@ export const useWindowDimension = (): number[] => {
     const debouncedResizeHandler = debounce(() => {
       /* console.log('***** debounced resize'); */ // See the cool difference in console
       setDimension([window.innerWidth, window.innerHeight]);
-    }, 100); // 100ms
+    }, 300); // 300ms
     window.addEventListener('resize', debouncedResizeHandler);
     return () => window.removeEventListener('resize', debouncedResizeHandler);
   }, []); // Note this empty array. this effect should run only on mount and unmount
   return dimension;
+};
+
+const debounce = (fn: () => void, ms: number): (() => void) => {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return function (): void {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn(), ms);
+  };
 };
